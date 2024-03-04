d161857e3b52ceddf4e15f39d2a0b53998014a2f
JVLAND <johnvincentland@gmail.com>
2024-03-04 15:09:33 +0800

    In the unnest_evidence stage, the infrequent possible values of multiclass evidences are tagged to "Others". Reducing 165 possible values to only top 10 per multiclass evidence. This reduced overall feature count from 900 to 280+

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 
 
 
-# Setup -------------------------------------------------------------------
+# 1. Setup -------------------------------------------------------------------
 
 # renv::activate()
 # renv::snapshot()
@@ -11,21 +11,21 @@ renv::restore()
 box::purge_cache()
 box::use(
   R/pull,
-  R/preparation
+  R/preparation,
 )
 box::reload(pull)
 box::reload(preparation)
 
 
-# Pull Datasets -----------------------------------------------------------
+# 2. Pull Datasets -----------------------------------------------------------
 
 
 pull$pull_conditions(pull_from_raw = TRUE)
 pull$pull_evidences(pull_from_raw = TRUE)
-train <- pull$pull_training(pull_from_raw = TRUE) # adds patient ID to 
+train <- pull$pull_training(pull_from_raw = TRUE) # adds patient ID to dataset
 
 
-# Preparation -------------------------------------------------------------
+# 3. Preparation -------------------------------------------------------------
 
 train_arrow <- arrow::open_dataset("data/processed/train.arrow", 
                                    format = "arrow")
@@ -34,7 +34,7 @@ box::help(preparation$unnest_evidences)
 
 
 train_sample <- train_arrow %>% 
-  slice_head(n = 100000) %>% 
+  slice_head(n = 10) %>% 
   collect() %>% 
   preparation$add_patient_ID()
 
@@ -42,10 +42,65 @@ train <- train_arrow %>%
   collect() %>% 
   preparation$add_patient_ID()
 
-(test <- preparation$unnest_evidences(train)) %>% system.time()
 
-test
+# 3.1. Unnest evidences ----------------------------------------------------
+
+start <- Sys.time()
+preparation$unnest_evidences_batch(
+  train, 
+  chunk_size = 100000,
+  result_path = "data/processed/prep1_unnestevidences.arrow"
+)
+end <- Sys.time()
+time <- difftime(end,start,units = "mins")
+time
+# Note: 
+# Currently, it takes 7-9 mins for all train rows
+# First attempt, I was able to reduce it to 5 mins
+# With chunking the data, I was able to reduce it to 3 mins
 
 
-## TODO
-# - speed up code, use dtplyr where possible
+
+# 3.2. Unnest differential diganosis --------------------------------------
+
+box::use(arrow[read_feather,write_feather],
+         purrr[iwalk])
+
+prep1 <- 
+  read_feather(file =
+                 "data/processed/prep1_unnestevidences.arrow")
+prep1_sample <- prep1 %>% 
+  slice_sample(n = 100000)
+
+
+start <- Sys.time()
+test <- 
+  preparation$unnest_differential(prep1_sample, count_diagnosis_keep = 100)
+end <- Sys.time()
+time <- difftime(end,start,units = "mins")
+time
+
+
+batches <- 
+  preparation$divide_data_to_batches(prep1, rows_per_batch = 100000)
+rm(prep1)
+gc()
+length(batches)
+start <- Sys.time()
+iwalk(batches, 
+      .f = function(x, idx){
+        output <- preparation$unnest_differential(x, 
+                                                  count_diagnosis_keep = 15)
+        arrow::write_feather(output, 
+                             sink = 
+                               paste0("data/processed/unnested_differential_batch",
+                                      idx,".arrow"))
+        rm(output)
+        gc()
+        
+      }, 
+      .progress = TRUE)
+end <- Sys.time()
+time <- difftime(end,start,units = "mins")
+time
+
